2d53e58a6c26c540ae51f010d24a7ddf8189c7db
mjavila4 <mjavila4@miners.utep.edu>
2022-02-20 13:16:32 -0700

    Created CD function

--- shell/shell.py
+++ shell/shell.py
@@ -8,21 +8,23 @@ prompt = Prompt()
 pid = os.getpid()
 rc = os.fork()
 
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
+while 1:
 
-elif rc == 0:
-    args = prompt.talk()
-    for dir in re.split(":", os.environ['PATH']):
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ)
-        except FileNotFoundError:
-            pass
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
 
-    os.write(2, (("Child:    Could not exec %s\n" % args[0]).encode()))
-    sys.exit(1)
+    elif rc == 0:
+        args = prompt.talk()
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:
+                pass
 
-else:
-    childPidCode = os.wait()
+        os.write(2, (("Child:    Could not exec %s\n" % args[0]).encode()))
+        sys.exit(1)
+
+    else:
+        childPidCode = os.wait()
