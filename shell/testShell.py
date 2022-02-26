2403869d008defe0cfdf1275c34ab09aa3c76091
mjavila4 <mjavila4@miners.utep.edu>
2022-02-26 15:09:45 -0700

    Created Exit function

--- shell/testShell.py
+++ shell/testShell.py
@@ -1,56 +1,30 @@
-import os, sys, re, fileinput
-from prompt import Prompt
+#! /usr/bin/env python3
 
-prompt = Prompt()
+import os, sys, time, re, fileinput
+
 pid = os.getpid()
 
 pr, pw = os.pipe()
-
 for f in (pr, pw):
     os.set_inheritable(f, True)
-
-args = prompt.talk()
-
-args1 = args[:args.index('|')]
-args2 = args[args.index('|')+1:]
 
 rc = os.fork()
 
 if rc == 0:
-
+    args = ["ls", "|", "sort"]
     os.close(1)
     os.dup(pw)
 
     for fd in (pr, pw):
         os.close(fd)
-
-    for dir in re.split(":", os.environ['PATH']):
-        program = "%s/%s" % (dir, args1[0])
-        try:
-            os.execve(program, args, os.environ)
-        except FileNotFoundError:
-            pass
-    sys.exit(1)
-
+        
+    print("Hello from child")
+    
 else:
-
-    pipeRC = os.fork()
-
-    if pipeRC == 0:
-        os.close(0)
-        os.dup(pr)
-        for fd in (pw, pr):
-            os.close(fd)
-
-        for dir in re.split(":", os.environ['PATH']):
-            program = "%s/%s" % (dir, args2[0])
-        try:
-            os.execve(program, args, os.environ)
-        except FileNotFoundError:
-            pass
-        sys.exit(1)
-
-    else:
-        os.wait()
-
-    childPidCode = os.wait()
+    
+    oa.close(0)
+    os.dup(pr)
+    for fd in (pw, pr):
+        os.close(fd)
+    line = os.read(pr, 100)
+    os.write(1, line)
