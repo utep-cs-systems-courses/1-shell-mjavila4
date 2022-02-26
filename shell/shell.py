f6dbfba907272ff9a445797146ed9aec189f54ca
mjavila4 <mjavila4@miners.utep.edu>
2022-02-26 15:01:30 -0700

    Created Exit function

--- shell/shell.py
+++ shell/shell.py
@@ -59,7 +59,7 @@ while 1:
                 os.dup(pr)
                 for fd in (pw, pr):
                     os.close(fd)
-                Exec.execProgram(args[args.index('|'):])
+                Exec.execProgram(args[args.index('|')+1:])
 
             else:
                 os.wait()
