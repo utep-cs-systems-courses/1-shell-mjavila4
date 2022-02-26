c80df39187e4ab14c4afe254fafafd2724811460
mjavila4 <mjavila4@miners.utep.edu>
2022-02-26 15:28:16 -0700

    Created Exit function

--- shell/testShell.py
+++ shell/testShell.py
@@ -3,6 +3,7 @@
 import os, sys, time, re, fileinput
 
 pid = os.getpid()
+args = ''
 
 pr, pw = os.pipe()
 for f in (pr, pw):
@@ -17,14 +18,35 @@ if rc == 0:
 
     for fd in (pr, pw):
         os.close(fd)
-        
-    print("Hello from child")
+
+    for dire in re.split(":", os.environ['PATH']):
+        program = "%s/%s" % (dire, args[0])
+        try:
+            os.execve(program, args[:args.index("|")], os.environ)
+        except FileNotFoundError:
+            pass
     
 else:
-    
-    os.close(0)
-    os.dup(pr)
-    line = os.read(pr, 100)
-    for fd in (pw, pr):
-        os.close(fd)
-    os.write(1, line)
+
+    rc2 = os.fork()
+
+    if rc2 == 0:
+
+        os.close(0)
+        os.dup(pr)
+
+        for dire in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dire, args[0])
+        try:
+            os.execve(program, args[args.index("|")+1:], os.environ)
+        except FileNotFoundError:
+            pass
+
+    else:
+        os.wait()
+        for fd in (pw, pr):
+            os.close(fd)
+
+os.wait()
+
+
